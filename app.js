391828e7255d431a3dfeb661895b95a0eb2ebd49
ruuffian <ruuffian@gmail.com>
2021-10-23 16:52:02 -0400

    footer css

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
 
 // Display Mobile Menu
 
 const mobileMenu = () => {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
-}
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+};
 
 // inline with arror functions (pass the function)
-menu.addEventListener('click', mobileMenu)
+menu.addEventListener("click", mobileMenu);
