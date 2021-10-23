71a77bd6a0d2ad685a6060dba7f1b2199c006e52
ruuffian <ruuffian@gmail.com>
2021-10-23 18:24:38 -0400

    fixed sign-up page scroll bug

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
+const navLogo = document.querySelector("#navbar__logo");
 
 // Display Mobile Menu
 
@@ -10,3 +11,9 @@ const mobileMenu = () => {
 
 // inline with arror functions (pass the function)
 menu.addEventListener("click", mobileMenu);
+
+// Show Active Menu When Scrolling
+
+const highlighMenu = () => {
+    
+}
