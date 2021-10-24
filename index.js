965b2816481cea1157fec4f1f73147e436be352a
ruuffian <ruuffian@gmail.com>
2021-10-23 21:17:21 -0400

    static html delivered

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const path = require('path');
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
-  });
+app.use(express.static('public'))
+
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname + "/index.html"));
+// });
 
 app.listen(port);
 console.log(`Server started at http://localhost:${port}`);
