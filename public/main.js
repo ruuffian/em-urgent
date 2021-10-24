4c7140c6c90cf0ee71e7ffbf0b872436ca2793f2
ruuffian <ruuffian@gmail.com>
2021-10-23 22:41:46 -0400

    functional text added, services page removed, moved from php to nodejs- ready to start working on adding more pages of html and eventually a rules engine

--- public/main.js
+++ public/main.js
@@ -13,29 +13,33 @@ const highlightMenu = () => {
   const element = document.querySelector(".highlight");
   const homeMenu = document.querySelector("#home-page");
   const aboutMenu = document.querySelector("#about-page");
-  const servicesMenu = document.querySelector("#services-page");
-  const signupMenu = document.querySelector("#signup");
+  // const servicesMenu = document.querySelector("#services-page");
+  const surveyMenu = document.querySelector("#survey");
 
   let scroll = window.scrollY;
-
+  
   if (window.innerWidth > 960) {
-    if (scroll < 600) {
+    if (scroll < 696) {
       homeMenu.classList.add("highlight");
       aboutMenu.classList.remove("highlight");
       return;
-    } else if (scroll < 1450) {
+    } else if (scroll < 1400) {
       homeMenu.classList.remove("highlight");
       aboutMenu.classList.add("highlight");
-      servicesMenu.classList.remove("highlight");
+      surveyMenu.classList.remove("highlight");
+      // servicesMenu.classList.remove("highlight");
       return;
-    } else if (scroll < 2473) {
+    }
+    // else if (scroll < 2473) {
+    //   aboutMenu.classList.remove("highlight");
+    //   signupMenu.classList.remove("highlight");
+    //   servicesMenu.classList.add("highlight");
+    //   return;
+    // }
+    else {
+      surveyMenu.classList.add("highlight");
       aboutMenu.classList.remove("highlight");
-      signupMenu.classList.remove("highlight");
-      servicesMenu.classList.add("highlight");
-      return;
-    } else {
-      signupMenu.classList.add("highlight");
-      servicesMenu.classList.remove("highlight");
+      // servicesMenu.classList.remove("highlight");
       return;
     }
   }
@@ -61,3 +65,7 @@ const hideMobileMenu = () => {
 
 menuLinks.addEventListener("click", hideMobileMenu);
 navLogo.addEventListener("click", hideMobileMenu);
+
+// Navigation Buttons
+
+// const
