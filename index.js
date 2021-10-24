95cb01dc63ca4c6af2ccb03120f0c593ea2602b0
ruuffian <ruuffian@gmail.com>
2021-10-23 21:25:23 -0400

    C:/Program Files/Git/home created as the mount

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const path = require('path');
+const path = require("path");
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.static('public'))
-
-// app.get("/", function (req, res) {
-//   res.sendFile(path.join(__dirname + "/index.html"));
-// });
+app.use("/home", express.static("public"));
 
 app.listen(port);
 console.log(`Server started at http://localhost:${port}`);
